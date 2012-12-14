2e63c61175af93929a230ca5df054ad1a95ab7f1
Marc Lagrange <rhaamo@sigpipe.me>
2012-12-14 20:40:36 +0100

    First import rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blusteromatic::Application.config.secret_token = 'd43633901988e74aba533b469760c4ec24f7a71d89603285ae446051332eae8d8b11b46c5ab6c32d531424085bb1bf8b5bb9cdcdffae430f68d51dccd8f56873'
