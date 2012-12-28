b1e937044b09122293697f11055e2a7740cf8ae8
Marc Lagrange <rhaamo@sigpipe.me>
2012-12-28 12:40:31 +0100

    Update node ruby with api call get_job

--- node-ruby/node.rb
+++ node-ruby/node.rb
@@ -43,6 +43,22 @@ end
 @thread_node_state = {:validated => false, :paused => false} # set default values
 
 
+def api_call(type, url, data)
+  if type == 'post'
+    req = Net::HTTP::Post.new(url, initheader = {'Content-Type' => 'application/json'})
+  elsif type == 'get'
+    req = Net::HTTP::Get.new(url, initheader = {'Content-Type' => 'application/json'})
+  end
+  req.body = data.to_json
+  begin
+    response = Net::HTTP.new(@config['api_host'], @config['api_port']).start { |http| http.request(req) }
+    return {:code => response.code, :message => response.message, :body => JSON.parse(response.body)}
+  rescue Errno::ECONNREFUSED
+    puts "[#{url}] could not connect to http://#{@config['api_host']}:#{@config['api_port']}"
+    return nil
+  end
+end
+
 
 # ===================================
 # Tread: HEARTBEAT
@@ -51,26 +67,19 @@ th_heartbeat = Thread.new do
   while true
     infos = @node_blendercfg
     infos['access_token'] = @config['api_token']
-    infos = infos.to_json
-    url = @config['api_heartbeat']
-    req = Net::HTTP::Post.new(url, initheader = {'Content-Type' => 'application/json'})
-    req.body = infos
-    begin
-      response = Net::HTTP.new(@config['api_host'], @config['api_port']).start { |http| http.request(req) }
-    rescue Errno::ECONNREFUSED
-      puts "[API][HEARTBEAT] could not connect to http://#{@config['api_host']}:#{@config['api_port']}"
+    hb_resp = api_call('post', @config['api_heartbeat'], infos)
+
+    if !hb_resp
       sleep 1
       next
     end
 
-    puts "[API][HEARTBEAT] response : #{response.code} #{response.message} : #{response.body}"
-
-    hb_resp = JSON.parse response.body
+    puts "[#{@config['api_heartbeat']}] response : #{hb_resp[:code]} #{hb_resp[:message]} : #{hb_resp[:body]}"
 
     semaphore.synchronize {
-      @thread_node_state[:validated] = hb_resp["validated"]
+      @thread_node_state[:validated] = hb_resp[:body]["validated"]
       @thread_node_state[:paused] = false
-      @thread_node_state[:id] = hb_resp["id"]
+      @thread_node_state[:id] = hb_resp[:body]["id"]
     }
     sleep 1
   end
@@ -79,16 +88,32 @@ end
 # ===================================
 # Tread: CPU
 # ===================================
-#th_job_cpu = Thread.new do
-#  while true
-#    st = nil
-#    semaphore.synchronize {
-#      st = validated
-#    }
-#    puts "CPU JOB: #{st}"
-#    sleep 10
-#  end
-#end
+th_job_cpu = Thread.new do
+  while true
+    validated, paused = nil
+    semaphore.synchronize {
+      validated = @thread_node_state[:validated]
+      paused = @thread_node_state[:paused]
+    }
+    if !validated or paused
+      puts "Node in pause or not validated."
+      sleep 10
+      next
+    end
+
+    infos = {:uuid => @node_blendercfg['uuid'], :compute => "CPU", :access_token => @config['api_token']}
+    cpu_resp = api_call('get', @config['api_get_job'], infos)
+
+    if !cpu_resp
+      sleep 10
+      next
+    end
+
+    puts "[#{@config['api_get_job']}] response : #{cpu_resp[:code]} #{cpu_resp[:message]} : #{cpu_resp[:body]}"
+
+    sleep 10
+  end
+end
 
 # ===================================
 # Tread: GPU
@@ -106,5 +131,5 @@ end
 
 # Start heartbeat thread
 th_heartbeat.join
-#th_job_cpu.join
+th_job_cpu.join
 #th_job_gpu.join
