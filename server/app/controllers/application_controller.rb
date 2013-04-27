8465f63aa174ce870e0afc15f8ea84b52faddbe2
Marc Lagrange <rhaamo@sigpipe.me>
2013-04-27 17:10:43 +0200

    Different root index for logged-in and not

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -2,6 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   check_authorization :unless => :ohai_can_you_please_check_if_the_controller_name_is_application_kthxbye?
 
+
+  def index
+    @jobs = Job.accessible_by(current_ability)
+    @nodes = Node.accessible_by(current_ability)
+
+    if current_user
+      render 'index'
+    else
+      render 'jobs/_list'
+    end
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
